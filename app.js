bb353076aa6b7e64c00977c2a4e2c1363e4833ba
osaretin frank <osaretin.frank10@gmail.com>
2022-08-05 19:08:43 +0100

    connected to db

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 require("dotenv").config()
 const express = require("express")
+const connectDB = require("./db/connect")
 const app = express()
 
 const PORT = process.env.PORT || 5500
 
 app.use(express.json())
 
-app.listen(PORT, () => {
-  console.log(`listening to port ${PORT}`)
-})
+const start = async () => {
+  try {
+    await connectDB()
+    app.listen(PORT, () => {
+      console.log(`listening to port ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+start()
